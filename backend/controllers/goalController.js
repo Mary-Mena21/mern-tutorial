4a344dd0138293addb10e88d2df353d3df5397c8
Mariana Mena <maranashokry@ahoo.com>
2024-04-03 11:38:40 -0500

    Goals controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,29 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Goal controller
 //@
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get goals!` });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async  (req, res) => {
     //console.log(req.body);
     if (req.body.text) {
         res.status(200).json({ message: `It Works!` });
     } else {
-        
-        res.status(400)//.json({ message: `Bad request!` });
+        res.status(400); //.json({ message: `Bad request!` });
         throw new Error(`Please add a text field!`);
     }
     res.status(200).json({ message: `Post goal!` });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler( async (req, res) => {
     res.status(200).json({ message: `Update goal!${req.params.id}` });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler( async (req, res) => {
     res.status(200).json({ message: `Delete goal!${req.params.id}` });
-};
+});
 module.exports = {
     getGoals,
     setGoal,
